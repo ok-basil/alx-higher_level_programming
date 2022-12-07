492a250af7686ff397f6ea0dc8b564707349235f
Okache "Al" Basil <69770152+ok-basil@users.noreply.github.com>
2022-12-07 16:10:33 +0100

    Trying something new here

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -4,7 +4,7 @@ def uniq_add(my_list=[]):
     sum_up = 0
     for i in range(len(my_list)):
         x = my_list[i]
-        [unique.append(x) for x in my_list if x not in unique]
+        [unique.append(x) for x in my_list: if x not in unique]
     for j in range(len(unique)):
         sum_up += unique[j]
     return 
